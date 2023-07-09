b4d0e0aa89cf72527f682b46969de6433cea7fbd
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2023-07-09 21:22:57 +0100

    E

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,16 +1,17 @@
 // Login.tsx
 import React from 'react';
-import styles from '../styles/login.module.css';
+import styles from '../styles/register.module.css';
 import Link from 'next/link';
 import { useState } from 'react';
 import axios from 'axios';
 import Image from 'next/image';
 import fourty from '../../public/bg.svg';
 import gog from '../../public/bg.svg';
-import Navbar from '@/components/navbar/Navbar';
+import Layout from '@/components/Layout/layout';
 
-export const Login = () => {
+export const Register = () => {
   const [data, setData] = useState({
+    username:'',
     email: '',
     password: '',
   });
@@ -27,7 +28,7 @@ export const Login = () => {
     postData(data);
   };
 
-  const postData = (data: { email: string; password: string }) => {
+  const postData = (data: { username:string; email: string; password: string}) => {
     axios
       .post('http://localhost:3000/api/login', data)
       .then((res: any) => console.log(res))
@@ -37,15 +38,28 @@ export const Login = () => {
   
   return (
     // < Navbar />
+    <Layout>
     <div className={styles.container}>
       <div className={styles.auth}>
-        <button className={styles.button}><Image className={styles.logo} alt="" src={fourty}/><div>Login with Intra</div></button>
-        <button className={styles.button}><Image className={styles.logoTwo} alt="" src={gog}/>Login with Google</button>
+        <button className={styles.button}><Image className={styles.logo} alt="" src={fourty}/><div>Register with Intra</div></button>
+        <button className={styles.button}><Image className={styles.logoTwo} alt="" src={gog}/>Register with Google</button>
         <div className={styles.or}>Or</div>
       </div>
       <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.infos}>
-          <h1 className={styles.title}>Log In</h1>
+          <h1 className={styles.title}>Sign Up</h1>
+          <label className={styles.label}>
+            Username:
+          </label>
+          <input
+            type="username"
+            placeholder="Username"
+            className={styles.input}
+            name="username"
+            value={data.username}
+            onChange={handleChange}
+            required
+          />
           <label className={styles.label}>
             Email:
           </label>
@@ -70,11 +84,12 @@ export const Login = () => {
             onChange={handleChange}
             required
           />
-          <button className={styles.logIn}>Login</button>
+          <button className={styles.logIn}>Register</button>
         </div>
       </form>
     </div>
+    </Layout>
   );
 };
 
-export default Login;
+export default Register;
